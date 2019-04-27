85ab90795667088c6dc5c10c321e5b373d78e3c4
Uzi Landsmann <uzi.landsmann@bisnode.com>
2019-04-27 18:21:54 +0200

    added go/isbn verifier

---
+++ isbn-verifier/isbn_verifier.go
@@ -0,0 +1,26 @@
+// Package isbn provides a solution to the ISBN Verifier exercise of the Go track in https://exercism.io
+package isbn
+
+import "strings"
+
+// IsValidISBN checks if the provided string is a valid ISBN-10
+func IsValidISBN(input string) bool {
+	cleaned := strings.ReplaceAll(input, "-", "")
+	if len(cleaned) != 10 {
+		return false
+	}
+
+	sum := 0
+	for i, r := range cleaned {
+		n := parseRune(i, r)
+		sum = sum + n*(10-i)
+	}
+	return sum%11 == 0
+}
+
+func parseRune(i int, r rune) int {
+	if i == 9 && r == 'X' {
+		return 10
+	}
+	return int(r - '0')
+}
