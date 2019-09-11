c87fd22f4b54f37a54175f5a076b9c5dac00aba4
Uzi Landsmann <uzi.landsmann@bisnode.com>
2019-09-11 21:00:14 +0200

    updated go/grains

--- grains/grains.go
+++ grains/grains.go
@@ -3,12 +3,7 @@ package grains
 
 import (
 	"errors"
-	"math/big"
 )
-
-var one = big.NewInt(1)
-var two = big.NewInt(2)
-var sixtyfour = big.NewInt(64)
 
 // Square calculates the number of grains of wheat on a chessboard given that the number on each square doubles.
 func Square(input int) (uint64, error) {
@@ -16,17 +11,10 @@ func Square(input int) (uint64, error) {
 		return 0, errors.New("wrong input")
 	}
 
-	var bigInput = big.NewInt(int64(input - 1))
-
-	var result big.Int
-	result.Exp(two, bigInput, nil)
-	return result.Uint64(), nil
+	return 1 << uint64(input-1), nil
 }
 
 // Total calculates the total number of grains of wheat on a chessboard given that the number on each square doubles.
 func Total() uint64 {
-	var result big.Int
-	result.Exp(two, sixtyfour, nil)
-	result.Sub(&result, one)
-	return result.Uint64()
+	return 1<<uint64(64) - 1
 }
