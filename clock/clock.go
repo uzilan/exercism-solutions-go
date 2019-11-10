6ae6c5f868b7d4bd86645d389ee734684362fda5
Uzi Landsmann <uzi.landsmann@bisnode.com>
2019-11-10 09:28:19 +0100

    updated go/clock

--- clock/clock.go
+++ clock/clock.go
@@ -5,48 +5,32 @@ import "fmt"
 
 // Clock implements a clock that handles times without dates
 type Clock struct {
-	hours   int
 	minutes int
 }
 
+const minutesPerDay = 1440
+
 // New creates a new clock with given hours and minutes.
-// If the minutes or the hours are not inside the normal intervals, they are converted to do so
 func New(hours int, minutes int) Clock {
-	if minutes >= 60 {
-		hours += minutes / 60
-		minutes %= 60
+	minutes += 60 * hours
+	minutes %= minutesPerDay
+	if minutes < 0 {
+		minutes += minutesPerDay
 	}
-
-	for minutes < 0 {
-		minutes += 60
-		hours--
-	}
-
-	if hours >= 24 {
-		hours %= 24
-	}
-
-	for hours < 0 {
-		hours += 24
-	}
-
-	return Clock{
-		hours,
-		minutes,
-	}
+	return Clock{minutes}
 }
 
 // String returns a textual representation of the clock in format hh:mm
 func (clock Clock) String() string {
-	return fmt.Sprintf("%02d:%02d", clock.hours, clock.minutes)
+	return fmt.Sprintf("%02d:%02d", clock.minutes/60, clock.minutes%60)
 }
 
 // Add adds given minutes to the clock
 func (clock Clock) Add(minutes int) Clock {
-	return New(clock.hours, clock.minutes+minutes)
+	return New(0, clock.minutes+minutes)
 }
 
 // Subtract subtracts given minutes from the clock
 func (clock Clock) Subtract(minutes int) Clock {
-	return New(clock.hours, clock.minutes-minutes)
+	return New(0, clock.minutes-minutes)
 }
